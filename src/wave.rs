7335df064262d83a50b0edb1a43a548786625ad2
Kurnevsky Evgeny <kurnevsky@gmail.com>
2016-02-28 16:24:51 +0300

    Small refactoring.

--- src/wave.rs
+++ src/wave.rs
@@ -14,7 +14,10 @@ impl Tag {
   }
 }
 
-pub fn wave<'r, T: Iterator<Item=&'r usize>, F1: FnMut(usize, usize, usize, usize) -> bool, F2: FnMut(usize, usize, usize, usize) -> bool>(width: usize, height: usize, tags: &mut Vec<Tag>, tagged: &mut Vec<usize>, start: &mut T, mut cond: F1, mut stop_cond: F2) -> Option<usize> {
+pub fn wave<'r, T, F1, F2>(width: usize, height: usize, tags: &mut Vec<Tag>, tagged: &mut Vec<usize>, start: &mut T, mut cond: F1, mut stop_cond: F2) -> Option<usize> where
+  T: Iterator<Item=&'r usize>,
+  F1: FnMut(usize, usize, usize, usize) -> bool,
+  F2: FnMut(usize, usize, usize, usize) -> bool {
   let mut q = VecDeque::new();
   for &pos in start {
     if cond(pos, pos, 0, pos) {
@@ -25,55 +28,56 @@ pub fn wave<'r, T: Iterator<Item=&'r usize>, F1: FnMut(usize, usize, usize, usiz
       tag.length = 1;
     }
   }
-  while !q.is_empty() {
-    let pos = q.pop_front().unwrap();
+  while let Some(pos) = q.pop_front() {
     tagged.push(pos);
-    let tag = tags[pos].clone();
-    if stop_cond(pos, tag.start, tag.length - 1, tag.prev) {
+    let length = tags[pos].length;
+    if stop_cond(pos, tags[pos].start, length - 1, tags[pos].prev) {
       return Some(pos);
     }
-    let start_pos = tag.start;
+    let start_pos = tags[pos].start;
     let n_pos = n(width, height, pos);
-    if tags[n_pos].length == 0 && cond(n_pos, start_pos, tag.length, pos) {
+    if tags[n_pos].length == 0 && cond(n_pos, start_pos, length, pos) {
       let n_tag = &mut tags[n_pos];
       n_tag.start = start_pos;
       n_tag.prev = pos;
-      n_tag.length = tag.length + 1;
+      n_tag.length = length + 1;
       tagged.push(n_pos);
       q.push_back(n_pos);
     }
     let w_pos = w(width, pos);
-    if tags[w_pos].length == 0 && cond(w_pos, start_pos, tag.length, pos) {
+    if tags[w_pos].length == 0 && cond(w_pos, start_pos, length, pos) {
       let w_tag = &mut tags[w_pos];
       w_tag.start = start_pos;
       w_tag.prev = pos;
-      w_tag.length = tag.length + 1;
+      w_tag.length = length + 1;
       tagged.push(w_pos);
       q.push_back(w_pos);
     }
     let s_pos = s(width, height, pos);
-    if tags[s_pos].length == 0 && cond(s_pos, start_pos, tag.length, pos) {
+    if tags[s_pos].length == 0 && cond(s_pos, start_pos, length, pos) {
       let s_tag = &mut tags[s_pos];
       s_tag.start = start_pos;
       s_tag.prev = pos;
-      s_tag.length = tag.length + 1;
+      s_tag.length = length + 1;
       tagged.push(s_pos);
       q.push_back(s_pos);
     }
     let e_pos = e(width, pos);
-    if tags[e_pos].length == 0 && cond(e_pos, start_pos, tag.length, pos) {
+    if tags[e_pos].length == 0 && cond(e_pos, start_pos, length, pos) {
       let e_tag = &mut tags[e_pos];
       e_tag.start = start_pos;
       e_tag.prev = pos;
-      e_tag.length = tag.length + 1;
+      e_tag.length = length + 1;
       tagged.push(e_pos);
       q.push_back(e_pos);
     }
   }
-  return None;
+  None
 }
 
-pub fn simple_wave<F1: FnMut(usize, usize, usize) -> bool, F2: FnMut(usize, usize, usize) -> bool>(width: usize, height: usize, tags: &mut Vec<Tag>, tagged: &mut Vec<usize>, start: usize, mut cond: F1, mut stop_cond: F2) -> Option<usize> {
+pub fn simple_wave<F1, F2>(width: usize, height: usize, tags: &mut Vec<Tag>, tagged: &mut Vec<usize>, start: usize, mut cond: F1, mut stop_cond: F2) -> Option<usize> where
+  F1: FnMut(usize, usize, usize) -> bool,
+  F2: FnMut(usize, usize, usize) -> bool {
   wave(width, height, tags, tagged, &mut Some(start).iter(), |pos, _, path_size, prev| { cond(pos, path_size, prev) }, |pos, _, path_size, prev| { stop_cond(pos, path_size, prev) })
 }
 
